8809f48504b63ef0036633a25d60062daca49471
LimYY07 <limyy-wm25@student.tarc.edu.my>
2026-03-08 21:17:09 +0800

    Student.java

---
+++ Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LYY
+ */
+
+class Student extends User {
+    private final String studentId;
+    public Student(int userId, String userName, String userEmail, String studentId) {
+        super(userId, userName, userEmail, "Student"); 
+        this.studentId = studentId;
+    }
+
+    @Override  //???
+    public String toString() {
+        return super.toString() + " | StudentID: " + studentId;
+    }
+}
