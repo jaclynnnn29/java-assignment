f61399d666fd38d4e5a2d3042c182fcef81e4906
LimYY07 <limyy-wm25@student.tarc.edu.my>
2026-03-08 20:53:09 +0800

    User.java

---
+++ User.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LYY
+ */
+
+class User {
+    private final int userId;
+    private String userName;
+    private String userEmail;
+    private final String userType;
+    
+    public User(int userId, String userName, String userEmail, String userType){
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userType = userType;
+    }
+
+    //Getters
+    public int getuserId() {
+        return userId;
+    }
+
+    public String getuserName() {
+        return userName;
+    }
+    
+    public String getuserEmail(){
+        return userEmail;
+    }
+    
+    //Setters
+    public void setuserName(String userName) {
+        this.userName = userName;
+    }
+    
+    public void setuserEmail(String userEmail) {
+        if (userEmail.contains("@")){
+            this.userEmail = userEmail;
+        } else {
+            System.out.println("Invalid Email lah aiyoo.");
+        }
+    }
+
+    public String toString() {
+        return String.format("User Type: %15s |ID: %-5d | Name: %-15s | Email: %-20s" , userType, userId, userName, userEmail);
+    }
+    
+}
