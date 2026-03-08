9a23f6b026f58f3fe66fe08e397223e414c58ff3
Ee Xiang <246417066+chanex-wm25-blip@users.noreply.github.com>
2026-03-08 20:28:12 +0800

    Still got problem

--- Catalog.java
+++ Catalog.java
@@ -8,26 +8,72 @@
  * @author ceexi
  */
 public class Catalog {
+    
     private String catelogID;
-    // private String LibraryItems[];
-    private String items;
+    private Resources[] LibraryItem;   
     private int totalItems;
-        
-    public Catalog(String catelogID,String items){
+            
+    public Catalog(String catelogID,int items){
         this.catelogID = catelogID;
-        this.items = items ;
-        //hvnt LibraryItems
+        this.LibraryItem = new Resources[items];
         this.totalItems = 0 ;
     }
-    
-    public addItems(String items){
-        items.add(items);
-        totalItems++
+    public void addItems(Resources item){
+        if (totalItems < LibraryItem.length){
+            LibraryItem[totalItems] = item;
+            totalItems++;
+        }else{
+            System.out.println("Your borrowing limit is fulled.");
+        }
     }
-    
-    public removeItems(String items){
-        items.add(items);
-        totalItems++
-    }
+    public void removeItems(String title){
+        for(int i=0 ; i < totalItems ; i++){
+            if(LibraryItem[i].getTitle().equalsIgnoreCase((title))){
+                
+                for(int j = i;j < totalItems - 1;j++){
+                    LibraryItem[j] = LibraryItem[j + 1];
+                }
+                LibraryItem[totalItems - 1] = null;
+                totalItems--;
+                break;
+            }
+        }
             
-}
+    }
+    public Resources searchByTitle(String title){
+        for (int i=0 ; i < totalItems ; i++){
+            if(LibraryItem[i].getTitle().equalsIgnoreCase(title)){
+                return LibraryItem[i];
+            }
+        }
+        System.out.println("Not Found");
+        return null;
+    }
+    public Resources searchById(String id){
+        for (int i=0 ; i < totalItems ; i++){
+            if(LibraryItem[i].getId().equalsIgnoreCase(id)){
+                return LibraryItem[i];
+            }
+        }
+        System.out.println("Not Found");
+        return null;
+    }
+    public Resources searchByGenre(String genre){
+        for (int i=0 ; i < totalItems ; i++){
+            if(LibraryItem[i].getId().equalsIgnoreCase(genre)){
+                return LibraryItem[i];
+            }
+        }
+        System.out.println("Not Found");
+        return null;
+    }
+    public int getTotalItems(){
+        return totalItems;
+    }
+    public void displayItems(){
+        for(int i=0 ; i < totalItems; i++){
+            System.out.println(LibraryItem[i]);
+        }
+    }
+}         
+
